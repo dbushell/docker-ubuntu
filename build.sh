32c56ef20943745d436db6516f000284879f5c62
David Bushell <david@dbushell.com>
2021-05-21 09:49:14 +0100

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker buildx build \
+docker buildx build --no-cache \
   -f ./Dockerfile --platform linux/arm64/v8,linux/x86_64 \
   -t dbushell/ubuntu \
   -t ghcr.io/dbushell/ubuntu \
