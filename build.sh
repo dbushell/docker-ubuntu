b7894588adce33c9d4c54baaa45ff86812fe4515
David Bushell <david@dbushell.com>
2022-06-25 16:39:12 +0100

    deno v1.23.1

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 docker buildx build --no-cache \
   -f ./Dockerfile --platform linux/arm64/v8,linux/x86_64 \
-  -t dbushell/ubuntu \
+  -t dbushell/ubuntu  \
   -t ghcr.io/dbushell/ubuntu \
   --push .
