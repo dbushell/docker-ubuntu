6e742b95f8a5eaab35d6701495bb7c003eea4eaa
David Bushell <david@dbushell.com>
2023-06-05 14:14:41 +0100

    fix deno url

--- devcontainer/features/deno/install.sh
+++ devcontainer/features/deno/install.sh
@@ -6,9 +6,9 @@ DENO_VERSION="${VERSION:-"1.34.1"}"
 
 # Fetch binary for architecture
 if [ "$(uname -m)" = "aarch64" ]; then
-  wget -c -O deno.zip "https://github.com/LukeChannings/deno-arm64/releases/download/${DENO_VERSION}/deno-linux-arm64.zip";
+  wget -c -O deno.zip "https://github.com/LukeChannings/deno-arm64/releases/download/v${DENO_VERSION}/deno-linux-arm64.zip";
 else
-  wget -c -O deno.zip "https://github.com/denoland/deno/releases/download/${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip";
+  wget -c -O deno.zip "https://github.com/denoland/deno/releases/download/v${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip";
 fi
 
 # Unzip and move binary
